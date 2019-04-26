bce3743011472927899d8dd0c2db038b7724d75b
anupnagarkoti <anup1007nagarkoti@gmail.com>
2019-04-26 08:34:55 +0545

    backend code commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const connection = require('express')
 const app = express();
 const path = require('path');
 const ejs = require('ejs');
+const multer = require('multer');
 
 
 app.use(connection.static(path.join(__dirname,'resources')))
@@ -16,6 +17,39 @@ app.get('/',(req,res)=>{
 
 res.render("index");
 
+});
+
+//set storage
+  const storage = multer.diskStorage({
+//passing object
+destination:'./resources/uploads/' ,
+//location where we want to upload the our image
+filename: function(req, file, cb)
+{
+	cb(null,file.originalname + '-' + Date.now() +
+		path.extname(file.originalname)); 
+}
+
+  });
+
+    const uploadImage = multer({
+  	storage: storage
+  	
+
+  }).single('image');
+
+    app.post('/imageupload',(req,res)=>
+{
+	uploadImage(req, res, (err) =>{
+if(err){
+	res.render('index',{
+		msg: err
+	} );
+}else {
+    res.send('image uploaded')
+    }
+
+	});
 });
 
 
